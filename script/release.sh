43344e46cf2ff677881a04e2d71798a72e28b904
yhwang <yhwang@che300.com>
2021-06-28 13:40:08 +0800

    [build] 1.1.2

---
+++ script/release.sh
@@ -0,0 +1,47 @@
+#! /bin/bash
+set -e
+
+git checkout master
+
+VERSION=`npx select-version-cli`
+
+read -p "Releasing $VERSION - are you sure? (y/n)" -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  echo "Releasing $VERSION ..."
+
+  # build
+  VERSION=$VERSION npm run build
+
+  # ssr test
+  # node test/ssr/require.test.js
+
+  # publish theme
+  # echo "Releasing theme-chalk $VERSION ..."
+  # cd packages/theme-chalk
+  # npm version $VERSION --message "[release] $VERSION"
+  # if [[ $VERSION =~ "beta" ]]
+  # then
+  #   npm publish --tag beta
+  # else
+  #   npm publish
+  # fi
+  # cd ../..
+
+  # commit
+  git add -A
+  git commit -m "[build] $VERSION"
+  npm version $VERSION --message "[release] $VERSION"
+
+  # publish
+  git push origin master
+  git push origin refs/tags/v$VERSION
+
+  if [[ $VERSION =~ "beta" ]]
+  then
+    npm publish --tag beta
+  else
+    npm publish
+  fi
+fi
