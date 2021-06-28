12d93cfcb04c5b503514ed88aa88db9f2280b4a1
wning <wning.neu_sr>
2021-06-28 09:45:14 +0800

    提交

--- website/router/index.js
+++ website/router/index.js
@@ -3,16 +3,17 @@ import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 
 const routes = [
+  { path: '/changelog', component: () => import('../../CHANGELOG.md') }
 ]
 importPages(require.context('../docs', true, /\.md$/, 'lazy'))
 function importPages(r) {
-    r.keys().forEach(key => {
-        routes.push({ path: (key.split('.'))[1], component: () => r(key) })
-    })
+  r.keys().forEach(key => {
+    routes.push({ path: (key.split('.'))[1], component: () => r(key) })
+  })
 }
 const router = new VueRouter({
-    base: process.env.BASE_URL,
-    routes,
+  base: process.env.BASE_URL,
+  routes
 })
 
 export default router
