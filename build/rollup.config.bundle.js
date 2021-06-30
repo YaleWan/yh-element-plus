de5ae95223f62de47e80725b85c2762d48ed0ebf
yhwang <yhwang@che300.com>
2021-06-30 16:35:37 +0800

    feat: 修改打包方式

--- build/rollup.config.bundle.js
+++ build/rollup.config.bundle.js
@@ -4,6 +4,8 @@ import { terser } from 'rollup-plugin-terser'
 import pkg from '../package.json'
 const deps = Object.keys(pkg.dependencies)
 const vue = require('rollup-plugin-vue')
+const commonjs = require('@rollup/plugin-commonjs')
+const json = require('@rollup/plugin-json')
 
 export default [
   {
@@ -13,12 +15,15 @@ export default [
       file: 'lib/index.esm.js'
     },
     plugins: [
-      terser(),
-      nodeResolve(),
+      nodeResolve({ jsnext: true, preferBuiltins: true, browser: true }),
+      commonjs({ browser: true }),
+      json(),
       vue({
         target: 'browser',
         css: false
-      })
+      }),
+      terser()
+
     ],
     external(id) {
       return /^vue/.test(id) ||
